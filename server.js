242d2eddd8139ee7df1f7d3348dcb27ab701cc5e
Vincent Behar <v.behar@free.fr>
2016-01-21 16:16:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello from OpenShift\n");
+  response.end("Hello from OpenShift!\n");
 }).listen(port);
 
 console.log("Server running on port " + port);
